ce36aa5102037772e9374854ed2135599ce3e03c
rockncoder <rockncoder@gmail.com>
2017-05-05 04:37:49 -0700

    Adds gh-stars, and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import * as os from 'os';
-import * as electron from 'electron';
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  eVersion = electron.remote.process.versions.electron;
-  title = `app works on ${os.platform()} with electron ${this.eVersion}!`;
+export class AppComponent implements OnInit {
+  title = 'GitHub Stars';
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppMaterialModule} from './app-material/app-material.module';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {GitHubService} from './git-hub.service';
+import {ResultsListComponent} from './results-list/results-list.component';
+import {MomentModule} from 'angular2-moment';
+import {Convert2KPipe} from './convert2k/convert2k.pipe';
+import {AboutComponent} from './about/about.component';
+import {AppRoutingModule} from './app-routing.module';
+import 'hammerjs';
+import 'moment';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ResultsListComponent,
+    Convert2KPipe,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule,
+    AppRoutingModule,
+    AppMaterialModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [GitHubService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
